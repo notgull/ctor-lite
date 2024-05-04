ac2e9dc919531a47bfcba2d28dd37273527156b7
John Nunley <dev@notgull.net>
2024-05-04 12:15:02 -0700

    chore: Fix some minor warts
    - Ensure global constants are `Sync`.
    - Add better formatting for large cfg() blocks.
    
    Signed-off-by: John Nunley <dev@notgull.net>

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,7 @@
 /// Run a function on program startup.
 #[macro_export]
 macro_rules! ctor {
+    // Case 1: Run a function at startup time.
     (
         $(#[$meta:meta])*
         $vis:vis unsafe fn $name:ident () $bl:block
@@ -17,23 +18,51 @@ macro_rules! ctor {
                 $bl
             }
 
-            #[cfg(not(any(target_os = "linux", target_os = "android", target_os = "freebsd", target_os = "netbsd", target_os = "openbsd", target_os = "dragonfly", target_os = "illumos", target_os = "haiku", target_os = "macos", target_os = "ios", target_os = "visionos", target_os = "tvos", windows)))]
+            #[cfg(not(any(
+                target_os = "linux",
+                target_os = "android",
+                target_os = "freebsd",
+                target_os = "netbsd",
+                target_os = "openbsd",
+                target_os = "dragonfly",
+                target_os = "illumos",
+                target_os = "haiku",
+                target_os = "macos",
+                target_os = "ios",
+                target_os = "visionos",
+                target_os = "tvos",
+                windows
+            )))]
             compile_error!("ctor! is not supported on the current target");
 
             #[used]
             #[allow(non_upper_case_globals, non_snake_case)]
             #[doc(hidden)]
-            #[cfg_attr(any(target_os = "linux", target_os = "android"), link_section = ".init_array")]
+            #[cfg_attr(
+                any(target_os = "linux", target_os = "android"),
+                link_section = ".init_array"
+            )]
             #[cfg_attr(target_os = "freebsd", link_section = ".init_array")]
             #[cfg_attr(target_os = "netbsd", link_section = ".init_array")]
             #[cfg_attr(target_os = "openbsd", link_section = ".init_array")]
             #[cfg_attr(target_os = "dragonfly", link_section = ".init_array")]
             #[cfg_attr(target_os = "illumos", link_section = ".init_array")]
             #[cfg_attr(target_os = "haiku", link_section = ".init_array")]
-            #[cfg_attr(any(target_os = "macos", target_os = "ios", target_os = "visionos", target_os = "tvos"), link_section = "__DATA,__mod_init_func")]
+            #[cfg_attr(
+                any(
+                    target_os = "macos",
+                    target_os = "ios",
+                    target_os = "visionos",
+                    target_os = "tvos"
+                ), 
+                link_section = "__DATA,__mod_init_func"
+            )]
             #[cfg_attr(windows, link_section = ".CRT$XCU")]
             static __rust_ctor_lite__ctor: unsafe extern "C" fn() -> usize = {
-                #[cfg_attr(any(target_os = "linux", target_os = "android"), link_section = ".text.startup")]
+                #[cfg_attr(
+                    any(target_os = "linux", target_os = "android"),
+                    link_section = ".text.startup"
+                )]
                 unsafe extern "C" fn ctor() -> usize {
                     $name ();
                     0
@@ -44,6 +73,7 @@ macro_rules! ctor {
         };
     };
 
+    // Case 2: Initialize a constant at bootup time.
     (
         $(#[$meta:meta])*
         $vis:vis unsafe static $(mut)? $name:ident:$ty:ty = $e:expr;
@@ -92,6 +122,11 @@ macro_rules! ctor {
                     }
                 }
             }
+
+            fn __assert_type_is_sync() {
+                fn __must_be_sync<T: Sync>() {}
+                __must_be_sync::<$ty>();
+            }
         };
     }
 }
@@ -112,7 +147,12 @@ macro_rules! dtor {
             }
 
             // Link directly to atexit in order to avoid a libc dependency.
-            #[cfg(not(any(target_os = "macos", target_os = "ios", target_os = "visionos", target_os = "tvos")))]
+            #[cfg(not(any(
+                target_os = "macos",
+                target_os = "ios",
+                target_os = "visionos",
+                target_os = "tvos"
+            )))]
             #[inline(always)]
             unsafe fn __do_atexit(cb: unsafe extern fn()) {
                 extern "C" {
@@ -122,20 +162,45 @@ macro_rules! dtor {
             }
 
             // For platforms that have __cxa_atexit, we register the dtor as scoped to dso_handle
-            #[cfg(any(target_os = "macos", target_os = "ios", target_os = "visionos", target_os = "tvos"))]
+            #[cfg(any(
+                target_os = "macos",
+                target_os = "ios",
+                target_os = "visionos",
+                target_os = "tvos"
+            ))]
             #[inline(always)]
             unsafe fn __do_atexit(cb: unsafe extern fn(_: *const u8)) {
                 extern "C" {
                     static __dso_handle: *const u8;
-                    fn __cxa_atexit(cb: unsafe extern fn(_: *const u8), arg: *const u8, dso_handle: *const u8);
+                    fn __cxa_atexit(
+                        cb: unsafe extern fn(_: *const u8),
+                        arg: *const u8,
+                        dso_handle: *const u8
+                    );
                 }
                 __cxa_atexit(cb, ::core::ptr::null(), __dso_handle);
             }
 
-            #[cfg(not(any(target_os = "macos", target_os = "ios", target_os = "visionos", target_os = "tvos")))]
-            #[cfg_attr(any(target_os = "linux", target_os = "android"), link_section = ".text.exit")]
+            #[cfg(not(any(
+                target_os = "macos",
+                target_os = "ios",
+                target_os = "visionos",
+                target_os = "tvos"
+            )))]
+            #[cfg_attr(
+                any(
+                    target_os = "linux",
+                    target_os = "android"
+                ),
+                link_section = ".text.exit"
+            )]
             unsafe extern "C" fn __run_destructor() { $name() };
-            #[cfg(any(target_os = "macos", target_os = "ios", target_os = "visionos", target_os = "tvos"))]
+            #[cfg(any(
+                target_os = "macos",
+                target_os = "ios",
+                target_os = "visionos",
+                target_os = "tvos"
+            ))]
             unsafe extern "C" fn __run_destructor(_: *const u8) { $name() };
 
             $crate::ctor! {
